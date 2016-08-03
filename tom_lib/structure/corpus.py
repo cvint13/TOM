4a1b9c2cef37c74716bea1a96ab941c5c23c38a0
Cynthia Vint <cvint13@live.com>
2016-08-03 13:52:59 -0400

    fixed corpus.py syntax error

--- tom_lib/structure/corpus.py
+++ tom_lib/structure/corpus.py
@@ -59,6 +59,50 @@ class Corpus:
         self.gensim_vector_space = None
         vocab = vectorizer.get_feature_names()
         self.vocabulary = dict([(i, s) for i, s in enumerate(vocab)])
+
+    def vector_for_document(self, doc_id):
+        vector = self.sklearn_vector_space[doc_id]
+        cx = vector.tocoo()
+        weights = [0.0] * len(self.vocabulary)
+        for row, word_id, weight in itertools.zip_longest(cx.row, cx.col, cx.data):
+            weights[word_id] = weight
+        return weights
+
+    def word_for_id(self, word_id):
+        return self.vocabulary.get(word_id)
+
+    def id_for_word(self, word):
+        for i, s in self.vocabulary.items():
+            if s == word:
+                return i
+        return -1
+
+    def similar_documents(self, doc_id, num_docs):
+        doc_weights = self.vector_for_document(doc_id)
+        similarities = []
+        for a_doc_id in range(self.size):
+            if a_doc_id != doc_id:
+                similarity = 1.0 - spatial.distance.cosine(doc_weights, self.vector_for_document(a_doc_id))
+                similarities.append((a_doc_id, similarity))
+        similarities.sort(key=lambda x: x[1], reverse=True)
+        return similarities[:num_docs]
+
+    def collaboration_network(self, doc_ids=None, nx_format=False):
+        nx_graph = nx.Graph(name='')
+        for doc_id in doc_ids:
+            authors = self.author(doc_id)
+            for author in authors:
+                nx_graph.add_node(author)
+            for i in range(0, len(authors)):
+                for j in range(i+1, len(authors)):
+                    nx_graph.add_edge(authors[i], authors[j])
+        bb = nx.betweenness_centrality(nx_graph)
+        nx.set_node_attributes(nx_graph, 'betweenness', bb)
+        if nx_format:
+            return nx_graph
+        else:
+            return json_graph.node_link_data(nx_graph)
+
 '''
     def export(self, file_path):
         self.data_frame.to_csv(path_or_buf=file_path, sep='\t', encoding='utf-8')
@@ -111,45 +155,3 @@ class Corpus:
     def doc_ids(self, date):
         return self.data_frame[self.data_frame['date'] == date].index.tolist()
 '''
-    def vector_for_document(self, doc_id):
-        vector = self.sklearn_vector_space[doc_id]
-        cx = vector.tocoo()
-        weights = [0.0] * len(self.vocabulary)
-        for row, word_id, weight in itertools.zip_longest(cx.row, cx.col, cx.data):
-            weights[word_id] = weight
-        return weights
-
-    def word_for_id(self, word_id):
-        return self.vocabulary.get(word_id)
-
-    def id_for_word(self, word):
-        for i, s in self.vocabulary.items():
-            if s == word:
-                return i
-        return -1
-
-    def similar_documents(self, doc_id, num_docs):
-        doc_weights = self.vector_for_document(doc_id)
-        similarities = []
-        for a_doc_id in range(self.size):
-            if a_doc_id != doc_id:
-                similarity = 1.0 - spatial.distance.cosine(doc_weights, self.vector_for_document(a_doc_id))
-                similarities.append((a_doc_id, similarity))
-        similarities.sort(key=lambda x: x[1], reverse=True)
-        return similarities[:num_docs]
-
-    def collaboration_network(self, doc_ids=None, nx_format=False):
-        nx_graph = nx.Graph(name='')
-        for doc_id in doc_ids:
-            authors = self.author(doc_id)
-            for author in authors:
-                nx_graph.add_node(author)
-            for i in range(0, len(authors)):
-                for j in range(i+1, len(authors)):
-                    nx_graph.add_edge(authors[i], authors[j])
-        bb = nx.betweenness_centrality(nx_graph)
-        nx.set_node_attributes(nx_graph, 'betweenness', bb)
-        if nx_format:
-            return nx_graph
-        else:
-            return json_graph.node_link_data(nx_graph)
